bd5a12ad5b06252300f2b832ee069bcf605f5134
Pvot <151098462+Pv0t@users.noreply.github.com>
2025-10-19 12:18:43 +0200

    Update FRSHardener.sh

--- FRSHardener.sh
+++ FRSHardener.sh
@@ -26,91 +26,94 @@ function improve_sshd {
 	echo "Protocol 2" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
 	echo "AddressFamily inet" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
 	echo "DenyUsers root" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+
+	echo -e "${GREEN}INPUT: ${NOCOLOR}Enter the username (excluding 'root') that you want to add to the 'AllowUsers' option: " 
+	read -p "" user
 	
-	read -p "Write the username of your user (not root) to put inside the 'AllowUsers' option: " users
+	echo "AllowUsers" $users | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#DenyGroups" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#AllowGroups" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "AuthorizedKeysFile .ssh/authorized_keys" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "Banner none" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
 	
-	echo "AllowUsers" $users | sudo tee -a /etc/ssh/sshd_config
-	echo "#DenyGroups" | sudo tee -a /etc/ssh/sshd_config
-	echo "#AllowGroups" | sudo tee -a /etc/ssh/sshd_config
-	echo "AuthorizedKeysFile .ssh/authorized_keys" | sudo tee -a /etc/ssh/sshd_config
-	echo "Banner none" | sudo tee -a /etc/ssh/sshd_config
-	
-	read -p "Do you wanna setup a 2FA authentication? [Y/N]" opt2
+	echo -e "${GREEN}INPUT: ${NOCOLOR}Do you wanna setup a 2FA authentication? [Y/N]"
+	read -p "" 2fa
 	#INSERT CASE HERE 
 	echo "ChallengeResponseAuthentication no"
 	echo "UsePAM no"
 
-	echo "ClientAliveInterval 120" | sudo tee -a /etc/ssh/sshd_config
-	echo "ClientAliveCountMax 1" | sudo tee -a /etc/ssh/sshd_config
-	echo "Compression delayed" | sudo tee -a /etc/ssh/sshd_config
-	echo "#ForceCommand" | sudo tee -a /etc/ssh/sshd_config
-	echo "GatewayPorts no" | sudo tee -a /etc/ssh/sshd_config
-	echo "LoginGraceTime 30" | sudo tee -a /etc/ssh/sshd_config
-	echo "LogLevel VERBOSE" | sudo tee -a /etc/ssh/sshd_config
-	echo "HostbasedAuthentication no" | sudo tee -a /etc/ssh/sshd_config
-	echo "HostbasedUsesNameFromPacketOnly" | sudo tee -a /etc/ssh/sshd_config
-	echo "IgnoreRhosts yes" | sudo tee -a /etc/ssh/sshd_config
-	echo "IgnoreUserKnownHosts yes" | sudo tee -a /etc/ssh/sshd_config
-	echo "MaxAuthTries 3" | sudo tee -a /etc/ssh/sshd_config
-	echo "MaxSessions 2" | sudo tee -a /etc/ssh/sshd_config
-	echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
-	echo "PermitEmptyPasswords no" | sudo tee -a /etc/ssh/sshd_config
-	echo "PermitRootLogin no" | sudo tee -a /etc/ssh/sshd_config
-	echo "PubkeyAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
-	echo "MaxStartups 5:30:30" | sudo tee -a /etc/ssh/sshd_config
-	
-	read -p "Write your Public IP: " your_ip
-	echo "ListenAddress "$your_ip":"$your_port | sudo tee -a /etc/ssh/sshd_config
-	echo "#ListenAddress 127.0.0.1:"$your_port "#For troubleshooting" | sudo tee -a /etc/ssh/sshd_config
+	echo "ClientAliveInterval 120" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "ClientAliveCountMax 1" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "Compression delayed" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#ForceCommand" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "GatewayPorts no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "LoginGraceTime 30" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "LogLevel VERBOSE" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "HostbasedAuthentication no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "HostbasedUsesNameFromPacketOnly" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "IgnoreRhosts yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "IgnoreUserKnownHosts yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "MaxAuthTries 3" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "MaxSessions 2" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "PermitEmptyPasswords no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "PermitRootLogin no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "PubkeyAuthentication yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "MaxStartups 5:30:30" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
 
-	echo "PermitUserEnvironment no" | sudo tee -a /etc/ssh/sshd_config
-	echo "#PidFile /var/run/sshd.pid #-> Default" | sudo tee -a /etc/ssh/sshd_config
-	echo "PrintLastLog no" | sudo tee -a /etc/ssh/sshd_config
-	echo "PrintMotd no" | sudo tee -a /etc/ssh/sshd_config
-	echo "#AuthorizedKeysCommand" | sudo tee -a /etc/ssh/sshd_config
-	echo "#AuthorizedKeysCommandUser" | sudo tee -a /etc/ssh/sshd_config
-	echo "#RequiredAuthentications" | sudo tee -a /etc/ssh/sshd_config
-	echo "#ShowPatchLevel" | sudo tee -a /etc/ssh/sshd_config
-	echo "StrictModes yes" | sudo tee -a /etc/ssh/sshd_config
-	echo "#Subsystem" | sudo tee -a /etc/ssh/sshd_config
-	echo "SyslogFacility AUTHPRIV" | sudo tee -a /etc/ssh/sshd_config
-	echo "TCPKeepAlive no" | sudo tee -a /etc/ssh/sshd_config
-	echo "UseDNS no" | sudo tee -a /etc/ssh/sshd_config
-	echo "ChrootDirectory none" | sudo tee -a /etc/ssh/sshd_config
-	echo "" | sudo tee -a /etc/ssh/sshd_config
-	echo "#===Cryptography===#" | sudo tee -a /etc/ssh/sshd_config
-	echo "HostKey /etc/ssh/ssh_host_ed25519_key" | sudo tee -a /etc/ssh/sshd_config
-	echo "Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com" | sudo tee -a /etc/ssh/sshd_config
-	echo "KexAlgorithms curve25519-sha256" | sudo tee -a /etc/ssh/sshd_config
-	echo "MACs hmac-sha2-512-etm@openssh.com" | sudo tee -a /etc/ssh/sshd_config
-	echo "HostKeyAlgorithms ssh-ed25519" | sudo tee -a /etc/ssh/sshd_config
-	echo "" | sudo tee -a /etc/ssh/sshd_config
-	echo "#===Forwarding===#" | sudo tee -a /etc/ssh/sshd_config
-	echo "PermitOpen none" | sudo tee -a /etc/ssh/sshd_config
-	echo "PermitTunnel no" | sudo tee -a /etc/ssh/sshd_config
-	echo "AllowAgentForwarding no" | sudo tee -a /etc/ssh/sshd_config
-	echo "AllowTcpForwarding no" | sudo tee -a /etc/ssh/sshd_config
-	echo "" | sudo tee -a /etc/ssh/sshd_config
-	echo "#===GSSAPI===#" | sudo tee -a /etc/ssh/sshd_config
-	echo "GSSAPIAuthentication no" | sudo tee -a /etc/ssh/sshd_config
-	echo "GSSAPIKeyExchange no" | sudo tee -a /etc/ssh/sshd_config 
-	echo "GSSAPICleanupCredentials yes" | sudo tee -a /etc/ssh/sshd_config
-	echo "GSSAPIStrictAcceptorCheck yes" | sudo tee -a /etc/ssh/sshd_config 
-	echo "GSSAPIStoreCredentialsOnRekey no" | sudo tee -a /etc/ssh/sshd_config
-	echo "" | sudo tee -a /etc/ssh/sshd_config
-	echo "#===Kerberos===#" | sudo tee -a /etc/ssh/sshd_config
-	echo "KerberosAuthentication no" | sudo tee -a /etc/ssh/sshd_config
-	echo "KerberosGetAFSToken no" | sudo tee -a /etc/ssh/sshd_config
-	echo "KerberosOrLocalPasswd yes" | sudo tee -a /etc/ssh/sshd_config
-	echo "KerberosTicketCleanup yes" | sudo tee -a /etc/ssh/sshd_config
-	echo "KerberosUseKuserok yes" | sudo tee -a /etc/ssh/sshd_config
-	echo "" | sudo tee -a /etc/ssh/sshd_config
-	echo "#===X11-Settings===#" | sudo tee -a /etc/ssh/sshd_config
-	echo "X11DisplayOffset 10" | sudo tee -a /etc/ssh/sshd_config
-	echo "X11Forwarding no" | sudo tee -a /etc/ssh/sshd_config
-	echo "X11UseLocalhost no" | sudo tee -a /etc/ssh/sshd_config 
-	echo "XAuthLocation none" | sudo tee -a /etc/ssh/sshd_config
-	echo "#============================" | sudo tee -a /etc/ssh/sshd_config   
+	echo -e "${GREEN}INPUT: ${NOCOLOR}Write your Public IP: "
+	read -p "" sshd_public_ip
+	echo "ListenAddress "$sshd_public_ip":"$sshd_port | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#ListenAddress 127.0.0.1:"$sshd_port "#For troubleshooting" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+
+	echo "PermitUserEnvironment no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#PidFile /var/run/sshd.pid #-> Default" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "PrintLastLog no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "PrintMotd no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#AuthorizedKeysCommand" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#AuthorizedKeysCommandUser" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#RequiredAuthentications" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#ShowPatchLevel" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "StrictModes yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#Subsystem" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "SyslogFacility AUTHPRIV" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "TCPKeepAlive no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "UseDNS no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "ChrootDirectory none" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#===Cryptography===#" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "HostKey /etc/ssh/ssh_host_ed25519_key" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "KexAlgorithms curve25519-sha256" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "MACs hmac-sha2-512-etm@openssh.com" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "HostKeyAlgorithms ssh-ed25519" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#===Forwarding===#" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "PermitOpen none" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "PermitTunnel no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "AllowAgentForwarding no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "AllowTcpForwarding no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#===GSSAPI===#" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "GSSAPIAuthentication no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "GSSAPIKeyExchange no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "GSSAPICleanupCredentials yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "GSSAPIStrictAcceptorCheck yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "GSSAPIStoreCredentialsOnRekey no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#===Kerberos===#" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "KerberosAuthentication no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "KerberosGetAFSToken no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "KerberosOrLocalPasswd yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "KerberosTicketCleanup yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "KerberosUseKuserok yes" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#===X11-Settings===#" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "X11DisplayOffset 10" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "X11Forwarding no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "X11UseLocalhost no" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "XAuthLocation none" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
+	echo "#============================" | sudo tee -a /etc/ssh/sshd_config 1>/dev/null
 }
 
 
